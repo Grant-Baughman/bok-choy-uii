4dc7b6dae69b67851d81d59a66249121f17af977
Grant Baughman <twbaugh2@gmail.com>
2022-02-04 11:53:03 -0500

    Initial Commit

---
+++ Shared/bok_choy_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  bok_choy_uiApp.swift
+//  Shared
+//
+//  Created by Grant Edward Baughman (Student) on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct bok_choy_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
