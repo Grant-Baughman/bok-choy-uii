408d46ebe8090c1084a6e7af302603fdfa3af2a7
Grant Baughman <twbaugh2@gmail.com>
2022-02-04 13:35:00 -0500

    swiftui thing first

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var circleColor = Color.red
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color.purple
+            
+            VStack{
+            Text("Hello, bok choy                         !")
+            
+            Text("Hello, bok choy !")
+        HStack{
+            
+            Text("you son of a ")
+                .font(.largeTitle)
+            RoundedRectangle(cornerRadius: 30 )
+            Text("Hello, bok choy!")
+    
+            
+        }
+        }
+            .font(.title)
+        }
+        
+        VStack{
+            Text("You change views with data")
+            Button("change color"){
+                self.circleColor = Color.green
+            }
+            Circle()
+                .foregroundColor( circleColor )
+        }
+        
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
